2ea13eef814cb89834f950826b99209596b8b336
Divash-Upadhyay <76831671+Divash-Upadhyay@users.noreply.github.com>
2022-03-22 23:42:03 +0530

    commit_

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
     return mongoose.connect(
-        ""
+        "mongodb+srv://dishu:qwerty123456@evaluation.ubmpk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     );
 }
